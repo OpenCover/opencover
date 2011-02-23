f831068007ae24d26e6c0783a97fef0332deeee3
Shaun Wilde <shaun_wilde@.rEm0vethIsB1t.hotmail.com>
2011-02-23 22:52:38 +1100

    * add basic console with all platforms * put all documentation output in a common folder

---
+++ main/OpenCover.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenCover.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
