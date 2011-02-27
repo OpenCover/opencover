4b3ade510412fc8c9cdc4566750bb5067d559be6
Shaun Wilde <shaun_wilde@.rEm0vethIsB1t.hotmail.com>
2011-02-28 00:09:22 +1100

    Simple registration of profiler using -register[:user]

--- main/OpenCover.Console/Program.cs
+++ main/OpenCover.Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
+using OpenCover.Framework;
 
 namespace OpenCover.Console
 {
@@ -10,8 +11,19 @@ namespace OpenCover.Console
     {
         static void Main(string[] args)
         {
+            var parser = new CommandLineParser(string.Join("", args));
+
             try
             {
+                if (parser.UserRegistration)
+                {
+                    Externals.DllInstall(1, "user");
+                }
+                else if (parser.Register)
+                {
+                     Externals.DllInstall(1, string.Empty);
+                }
+
                 var startInfo = new ProcessStartInfo(Path.Combine(Environment.CurrentDirectory, "OpenCover.Simple.Target.exe"));
                 startInfo.EnvironmentVariables.Add("Cor_Profiler", "{1542C21D-80C3-45E6-A56C-A9C1E4BEB7B8}");
                 startInfo.EnvironmentVariables.Add("Cor_Enable_Profiling", "1");
@@ -24,7 +36,14 @@ namespace OpenCover.Console
             }
             finally
             {
-                
+                if (parser.UserRegistration)
+                {
+                    Externals.DllInstall(0, "user");
+                }
+                else if (parser.Register)
+                {
+                    Externals.DllInstall(0, string.Empty);
+                }
             }
         }
     }
