eee853e1a70b18861332a6c08ef6b63edb922f95
Shaun Wilde <shaun_wilde@.rEm0vethIsB1t.hotmail.com>
2011-04-16 09:49:40 +1000

    update build scripts

--- build/Version/Version.cs
+++ build/Version/Version.cs
@@ -1,1 +1,1 @@
-using System.Reflection;[assembly: AssemblyVersion("1.0.112.13")][assembly: AssemblyFileVersion("1.0.112.13")]
+using System.Reflection;[assembly: AssemblyVersion("1.0.216.3")][assembly: AssemblyFileVersion("1.0.216.3")]
