4333ef15f7cc29d68eac2ec92e1f62299c95db32
sawilde <shaun_wilde@hotmail.com>
2011-07-06 22:07:55 +1000

    update version number and apply filter fixes to dogfood tests

--- build/Version/Version.cs
+++ build/Version/Version.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("1.0.421.0")]
-[assembly: AssemblyFileVersion("1.0.421.0")]
+[assembly: AssemblyVersion("1.0.506.1")]
+[assembly: AssemblyFileVersion("1.0.506.1")]
