05be1cd21d4de9af5dea659766dd02349691a67c
sawilde <shaun_wilde@hotmail.com>
2011-07-08 23:08:03 +1000

    update version

--- build/Version/Version.cs
+++ build/Version/Version.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("1.0.506.1")]
-[assembly: AssemblyFileVersion("1.0.506.1")]
+[assembly: AssemblyVersion("1.0.508.0")]
+[assembly: AssemblyFileVersion("1.0.508.0")]
--- build/Version/Version.h
+++ build/Version/Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        1,0,506,1
-#define PRODUCTVER        1,0,506,1
-#define STRFILEVER        "1.0.506.1"
-#define STRPRODUCTVER        "1.0.506.1"
+#define FILEVER        1,0,508,0
+#define PRODUCTVER        1,0,508,0
+#define STRFILEVER        "1.0.508.0"
+#define STRPRODUCTVER        "1.0.508.0"
