763f7fc658a6c12f8254500d89c17cf2e980502a
sawilde <shaun_wilde@hotmail.com>
2011-08-17 23:11:36 +1000

    update nuget package details

--- build/Version/Version.cs
+++ build/Version/Version.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("1.0.615.2")]
-[assembly: AssemblyFileVersion("1.0.615.2")]
+[assembly: AssemblyVersion("1.0.617.1")]
+[assembly: AssemblyFileVersion("1.0.617.1")]
