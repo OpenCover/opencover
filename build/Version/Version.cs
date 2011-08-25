8803ac352c0b9c47aaf55994012430d95b344c9e
sawilde <shaun_wilde@hotmail.com>
2011-08-25 23:02:39 +1000

    update to latest version of ReportGenerator

--- build/Version/Version.cs
+++ build/Version/Version.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("1.0.621.2")]
-[assembly: AssemblyFileVersion("1.0.621.2")]
+[assembly: AssemblyVersion("1.0.625.0")]
+[assembly: AssemblyFileVersion("1.0.625.0")]
--- build/Version/Version.h
+++ build/Version/Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        1,0,621,2
-#define PRODUCTVER        1,0,621,2
-#define STRFILEVER        "1.0.621.2"
-#define STRPRODUCTVER        "1.0.621.2"
+#define FILEVER        1,0,625,0
+#define PRODUCTVER        1,0,625,0
+#define STRFILEVER        "1.0.625.0"
+#define STRPRODUCTVER        "1.0.625.0"
