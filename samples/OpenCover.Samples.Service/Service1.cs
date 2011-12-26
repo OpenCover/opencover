1057a9345a4cd0c090ce76c205f0588de198e5c9
sawilde <shaun_wilde@hotmail.com>
2011-12-27 07:45:00 +1100

    Create sample service for use with testing #36

---
+++ samples/OpenCover.Samples.Service/Service1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using OpenCover.Samples.CS;
+
+namespace OpenCover.Samples.Service
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            var target = new TryFinallyTarget(new CustomExceptionQuery());
+            target.TryFinally();
+        }
+
+        protected override void OnStop()
+        {
+            var target = new TryExceptionTarget(new CustomExceptionQuery());
+            target.TryException();
+        }
+    }
+}
