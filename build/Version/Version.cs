25ec3bf19c577bdacc0326df3484f437e9fe1fe9
sawilde <shaun_wilde@hotmail.com>
2012-01-15 20:30:16 +1100

    switch from task_group which keeps locking and use old-faithful threads #55

--- build/Version/Version.cs
+++ build/Version/Version.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("2.0.6.0")]
-[assembly: AssemblyFileVersion("2.0.6.0")]
+[assembly: AssemblyVersion("2.1.15.3")]
+[assembly: AssemblyFileVersion("2.1.15.3")]
