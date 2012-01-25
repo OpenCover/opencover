83c2f284e6756895ed38df6101489a4598bf108c
sawilde <shaun_wilde@hotmail.com>
2012-01-25 22:29:23 +1100

    update version

--- build/Version/Version.h
+++ build/Version/Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,1,15,3
-#define PRODUCTVER        2,1,15,3
-#define STRFILEVER        "2.1.15.3"
-#define STRPRODUCTVER        "2.1.15.3"
+#define FILEVER        2,1,25,3
+#define PRODUCTVER        2,1,25,3
+#define STRFILEVER        "2.1.25.3"
+#define STRPRODUCTVER        "2.1.25.3"
