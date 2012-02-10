860e3fd628a2d95623c8948a8c7de60cd499dc76
sawilde <shaun_wilde@hotmail.com>
2012-02-11 06:50:24 +1100

    update version

--- build/Version/Version.cs
+++ build/Version/Version.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("2.1.25.3")]
-[assembly: AssemblyFileVersion("2.1.25.3")]
+[assembly: AssemblyVersion("2.1.111.1")]
+[assembly: AssemblyFileVersion("2.1.111.1")]
