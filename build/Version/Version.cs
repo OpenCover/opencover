f187d03647d03c067f04b217510a7e38e504e2fe
sawilde <shaun_wilde@hotmail.com>
2012-04-01 12:12:08 +1000

    Add some start menu entries #83

--- build/Version/Version.cs
+++ build/Version/Version.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("4.0.217.0")]
-[assembly: AssemblyFileVersion("4.0.217.0")]
+[assembly: AssemblyVersion("4.0.301.10")]
+[assembly: AssemblyFileVersion("4.0.301.10")]
--- build/Version/Version.h
+++ build/Version/Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        4,0,217,0
-#define PRODUCTVER        4,0,217,0
-#define STRFILEVER        "4.0.217.0"
-#define STRPRODUCTVER        "4.0.217.0"
+#define FILEVER        4,0,301,10
+#define PRODUCTVER        4,0,301,10
+#define STRFILEVER        "4.0.301.10"
+#define STRPRODUCTVER        "4.0.301.10"
