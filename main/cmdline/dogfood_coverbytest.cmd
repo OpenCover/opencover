4b9032a5d9e6e93391da107ab607ea1a9d983b7d
sawilde <shaun_wilde@hotmail.com>
2012-05-23 23:08:03 +1000

    fix command scripts

--- main/cmdline/dogfood_coverbytest.cmd
+++ main/cmdline/dogfood_coverbytest.cmd
@@ -1,1 +1,1 @@
-OpenCover.Console.exe -register:user -target:..\..\..\tools\NUnit-2.6.0.12051\bin\net-2.0\nunit-console-x86.exe -targetargs:"OpenCover.Test.dll /noshadow" -filter:"+[Open*]* -[OpenCover.T*]*" -coverbytest:*.Test.dll -output:opencovertests.xml
+OpenCover.Console.exe -register:user -target:..\..\..\tools\NUnit-2.6.0.12051\bin\nunit-console-x86.exe -targetargs:"OpenCover.Test.dll /noshadow" -filter:"+[Open*]* -[OpenCover.T*]*" -coverbytest:*.Test.dll -output:opencovertests.xml
