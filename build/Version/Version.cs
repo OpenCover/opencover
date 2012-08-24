3f143aee18200c3a7b4c0d9d2af8f7aa61776ec8
sawilde <shaun_wilde@hotmail.com>
2012-08-24 22:29:09 +1000

    new merged build

--- build/Version/Version.cs
+++ build/Version/Version.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("4.0.724.3")]
-[assembly: AssemblyFileVersion("4.0.724.3")]
+[assembly: AssemblyVersion("4.0.724.4")]
+[assembly: AssemblyFileVersion("4.0.724.4")]
--- build/Version/Version.h
+++ build/Version/Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        4,0,724,3
-#define PRODUCTVER        4,0,724,3
-#define STRFILEVER        "4.0.724.3"
-#define STRPRODUCTVER        "4.0.724.3"
+#define FILEVER        4,0,724,4
+#define PRODUCTVER        4,0,724,4
+#define STRFILEVER        "4.0.724.4"
+#define STRPRODUCTVER        "4.0.724.4"
