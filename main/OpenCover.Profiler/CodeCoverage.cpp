4c278e701dab27391e8cc437d7edeb5cc111d591
sawilde <shaun_wilde@hotmail.com>
2013-01-27 22:14:19 +1100

    overview of changes (rejit-unhook) fix command line build

--- main/OpenCover.Profiler/CodeCoverage.cpp
+++ main/OpenCover.Profiler/CodeCoverage.cpp
@@ -96,8 +96,8 @@ HRESULT STDMETHODCALLTYPE CCodeCoverage::Initialize(
 	if (m_profilerInfo4 != NULL)
 	{
         ATLTRACE(_T("    ::Initialize (m_profilerInfo4 OK)"));
-		dwMask |= COR_PRF_ENABLE_REJIT;
-		dwMask |= COR_PRF_DISABLE_ALL_NGEN_IMAGES;
+		//dwMask |= COR_PRF_ENABLE_REJIT;
+		//dwMask |= COR_PRF_DISABLE_ALL_NGEN_IMAGES;
 	}
 
     COM_FAIL_MSG_RETURN_ERROR(m_profilerInfo2->SetEventMask(dwMask), 
