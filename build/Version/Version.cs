7b945c406e5ee29b2b686b8d625324d3f13093e8
sawilde <shaun_wilde@hotmail.com>
2013-01-29 23:06:29 +1100

    update release

--- build/Version/Version.cs
+++ build/Version/Version.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("4.0.1128.0")]
-[assembly: AssemblyFileVersion("4.0.1128.0")]
+[assembly: AssemblyVersion("4.0.1229.0")]
+[assembly: AssemblyFileVersion("4.0.1229.0")]
--- build/Version/Version.h
+++ build/Version/Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        4,0,1128,0
-#define PRODUCTVER        4,0,1128,0
-#define STRFILEVER        "4.0.1128.0"
-#define STRPRODUCTVER        "4.0.1128.0"
+#define FILEVER        4,0,1229,0
+#define PRODUCTVER        4,0,1229,0
+#define STRFILEVER        "4.0.1229.0"
+#define STRPRODUCTVER        "4.0.1229.0"
