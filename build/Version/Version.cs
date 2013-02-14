3a03079c5711b8779f4d5f0b78d8043346ea201a
sawilde <shaun_wilde@hotmail.com>
2013-02-14 18:46:14 +1100

    add missing autofac assembly to nuget/zip packages #148

--- build/Version/Version.cs
+++ build/Version/Version.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("4.5.1313.6")]
-[assembly: AssemblyFileVersion("4.5.1313.6")]
+[assembly: AssemblyVersion("4.5.1314.1")]
+[assembly: AssemblyFileVersion("4.5.1314.1")]
--- build/Version/Version.h
+++ build/Version/Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        4,5,1313,6
-#define PRODUCTVER        4,5,1313,6
-#define STRFILEVER        "4.5.1313.6"
-#define STRPRODUCTVER        "4.5.1313.6"
+#define FILEVER        4,5,1314,1
+#define PRODUCTVER        4,5,1314,1
+#define STRFILEVER        "4.5.1314.1"
+#define STRPRODUCTVER        "4.5.1314.1"
