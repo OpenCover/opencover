103a1046cdb40a4ad2efe79089e5328a8a363669
sawilde <shaun_wilde@hotmail.com>
2013-05-04 14:16:29 +1000

    update build number

--- build/Version/Version.h
+++ build/Version/Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        4,5,1528,0
-#define PRODUCTVER        4,5,1528,0
-#define STRFILEVER        "4.5.1528.0"
-#define STRPRODUCTVER        "4.5.1528.0"
+#define FILEVER        4,5,1604,0
+#define PRODUCTVER        4,5,1604,0
+#define STRFILEVER        "4.5.1604.0"
+#define STRPRODUCTVER        "4.5.1604.0"
