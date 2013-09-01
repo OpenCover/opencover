bd494457a7c8a8cdbcb5bf3d1f4ead0283e73381
sawilde <shaun_wilde@hotmail.com>
2013-09-01 20:33:22 +1000

    create release

--- build/Version/Version.cs
+++ build/Version/Version.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("4.5.1923.1")]
-[assembly: AssemblyFileVersion("4.5.1923.1")]
+[assembly: AssemblyVersion("4.5.2001.0")]
+[assembly: AssemblyFileVersion("4.5.2001.0")]
--- build/Version/Version.h
+++ build/Version/Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        4,5,1923,1
-#define PRODUCTVER        4,5,1923,1
-#define STRFILEVER        "4.5.1923.1"
-#define STRPRODUCTVER        "4.5.1923.1"
+#define FILEVER        4,5,2001,0
+#define PRODUCTVER        4,5,2001,0
+#define STRFILEVER        "4.5.2001.0"
+#define STRPRODUCTVER        "4.5.2001.0"
