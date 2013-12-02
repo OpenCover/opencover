81375dc1ff709e5c8ad9edc80c149b41a8a7fb47
Dragan <github@registratura.org>
2013-12-02 22:37:58 +0100

    Branch-Point attributes extended (coverage.xml)
    Branch-Point path is tracked from existing Offset via (new) Offset-Chain
    to (new) Offset-End attribute. In example:
    <BranchPoint vc="0" uspid="13378" ordinal="8" offset="108" path="0"
    offsetchain="121 143" offsetend="148" />

--- main/OpenCover.Framework/Model/BranchPoint.cs
+++ main/OpenCover.Framework/Model/BranchPoint.cs
@@ -12,5 +12,16 @@ namespace OpenCover.Framework.Model
         /// </summary>
         [XmlAttribute("path")]
         public int Path { get; set; }
+        /// <summary>
+        /// List of OffsetPoints between Offset and EndOffset (exclusive)
+        /// </summary>
+        [XmlAttribute("offsetchain")]
+        public System.Collections.Generic.List<int> OffsetPoints { get; set; }
+        /// <summary>
+        /// Last Offset == EndOffset.
+        /// Can be same as Offset
+        /// </summary>
+        [XmlAttribute("offsetend")]
+        public int EndOffset { get; set; }
     }
 }
