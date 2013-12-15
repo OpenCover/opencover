46c82b920c8dd66bcbc3f435f1c48702723f3b24
sawilde <shaun_wilde@hotmail.com>
2013-12-16 06:39:12 +1100

    re-enable COR_PRF_DISABLE_ALL_NGEN_IMAGES to ignore all NGENd images (even NGEN /Profile images) #188

--- main/OpenCover.Profiler/CodeCoverage.cpp
+++ main/OpenCover.Profiler/CodeCoverage.cpp
@@ -116,8 +116,7 @@ HRESULT STDMETHODCALLTYPE CCodeCoverage::Initialize(
     if (m_profilerInfo4 != NULL)
     {
         ATLTRACE(_T("    ::Initialize (m_profilerInfo4 OK)"));
-        //dwMask |= COR_PRF_ENABLE_REJIT;
-        //dwMask |= COR_PRF_DISABLE_ALL_NGEN_IMAGES;
+        dwMask |= COR_PRF_DISABLE_ALL_NGEN_IMAGES;
     }
 #endif
     COM_FAIL_MSG_RETURN_ERROR(m_profilerInfo2->SetEventMask(dwMask), 
