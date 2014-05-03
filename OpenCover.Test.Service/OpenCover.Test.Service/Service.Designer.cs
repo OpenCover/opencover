1216ee490684246760770c542572c560507b0b55
SteveGilham <SteveGilham@users.noreply.github.com>
2014-05-03 16:09:40 +0100

    Skeleton service running as NETWORK SERVICE
    Trival service based on the walkthrough at
    http://msdn.microsoft.com/en-us/library/zt39148a%28v=vs.110%29.aspx
    Uses Debug.WriteLine rather than Event Log to avoid the need to set
    access rights for the relevant keys
    Includes PowerShell script for file ACL setting, a script of manual test
    steps, and sample coverage output

---
+++ OpenCover.Test.Service/OpenCover.Test.Service/Service.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace OpenCover.Test.Service
+{
+    partial class Service
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.eventLog = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog)).BeginInit();
+            // 
+            // Service
+            // 
+            this.ServiceName = "Service1";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.EventLog eventLog;
+    }
+}
---
+++ OpenCover.Test.Service/OpenCover.Test.Service/Service.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenCover.Test.Service
+{
+    /// <summary>
+    /// Trival service based on the walkthrough at http://msdn.microsoft.com/en-us/library/zt39148a%28v=vs.110%29.aspx
+    /// Uses Debug.WriteLine rather than Event Log to avoid the need to set access rights for the relevant keys
+    /// Follow the service activity by running DebugView -- http://technet.microsoft.com/en-gb/sysinternals/bb896647.aspx -- as 
+    /// Administrator and Capture Global Win32 traces
+    /// </summary>
+    public partial class Service : ServiceBase
+    {
+        private AutoResetEvent waiter;
+
+        /// <summary>
+        /// Initializes a new instance of the Service class
+        /// </summary>
+        public Service()
+        {
+            InitializeComponent();
+            waiter = new AutoResetEvent(false);
+            CanShutdown = true;
+            CanStop = true;
+            Debug.WriteLine("Constructed service");
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new Service() 
+            };
+
+            ServiceBase.Run(ServicesToRun);
+        }
+
+        /// <summary>
+        /// Called by the SCM when a service start is sent and starts 
+        /// the service payload as an asynchronous operation
+        /// </summary>
+        /// <param name="args">This parameter is not used.</param>
+        protected override void OnStart(string[] args)
+        {
+            Debug.WriteLine("Starting service");
+            Task.Run(() =>
+            {
+                // Heartbeat at 5 second intervals until signalled to stop
+                var interval = new TimeSpan(0, 0, 5);
+                while (!waiter.WaitOne(interval))
+                {
+                    Debug.WriteLine("Service working");
+                }
+
+                Debug.WriteLine("Service exiting");
+            });
+        }
+
+        /// <summary>
+        /// Called by the SCM when a service stop is sent
+        /// </summary>
+        protected override void OnStop()
+        {
+            Debug.WriteLine("Stopping service");
+            waiter.Set();
+        }
+
+        /// <summary>
+        /// Called by the SCM when the system is shutting down
+        /// </summary>
+        protected override void OnShutdown()
+        {
+            eventLog.WriteEntry("Shutting down service");
+            waiter.Set();
+        }
+    }
+}
