989ac625ad62232cbefe3e3c9f57f486874c9123
sawilde <shaun_wilde@hotmail.com>
2014-09-06 18:31:12 +1000

    #209 batch handles into blocks of 32 so as to not reach the 64 limit of WaitHandles.WaitAll();

--- main/OpenCover.Framework/Manager/IProfilerManager.cs
+++ main/OpenCover.Framework/Manager/IProfilerManager.cs
@@ -4,13 +4,20 @@
 // This source code is released under the MIT License; see the accompanying license file.
 //
 using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
 
 namespace OpenCover.Framework.Manager
 {
+    /// <summary>
+    /// Describe the external interaction with the profiler manager
+    /// </summary>
     public interface IProfilerManager
     {
-        void RunProcess(Action<Action<StringDictionary>> process, IEnumerable<string> servicePrincipal);
+        /// <summary>
+        /// Let's start profiling
+        /// </summary>
+        /// <param name="process"></param>
+        /// <param name="servicePrincipal"></param>
+        void RunProcess(Action<Action<StringDictionary>> process, string[] servicePrincipal);
     }
 }
