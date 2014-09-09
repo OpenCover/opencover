69759c45d33c0dbd0913a943022c80d12061ad70
sawilde <shaun_wilde@hotmail.com>
2014-09-09 21:45:51 +1000

    #218 auto generate release notes to be tagged with build version

--- build/Version/Version.cs
+++ build/Version/Version.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("4.5.2822.0")]
-[assembly: AssemblyFileVersion("4.5.2822.0")]
+[assembly: AssemblyVersion("4.5.3209.8")]
+[assembly: AssemblyFileVersion("4.5.3209.8")]
