4e0c9d4218bc5eb09bf41ccf8f503476714963d7
sawilde <shaun_wilde@hotmail.com>
2014-11-07 08:19:18 +1100

    retool to latest nuget packages

--- main/cmdline/opencovertests.cmd
+++ main/cmdline/opencovertests.cmd
@@ -1,1 +1,1 @@
-..\..\..\main\packages\NUnit.Runners.2.6.2\tools\nunit-console-x86.exe OpenCover.Test.dll /noshadow /exclude:AdminOnly
+..\..\..\main\packages\NUnit.Runners.2.6.3\tools\nunit-console-x86.exe OpenCover.Test.dll /noshadow /exclude:AdminOnly
