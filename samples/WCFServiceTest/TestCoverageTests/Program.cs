7ead5e57001c5034e256e58174a9f67497bd6f57
AndiVlad <mihai.vlad@gmail.com>
2014-11-20 11:55:31 +0200

    Added sample WCF service

---
+++ samples/WCFServiceTest/TestCoverageTests/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestCoverageTests.ServiceReference1;
+
+namespace TestCoverageTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Service1Client srv = new Service1Client();
+                var contact = srv.GetDataUsingDataContract(new CompositeType() { BoolValue = true, StringValue = "xxx" });
+                Console.WriteLine(contact.StringValue);
+                srv.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
