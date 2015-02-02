9dad00cd77766f0b1c3141853277068ae6d2cca4
sawilde <shaun_wilde@hotmail.com>
2015-02-02 23:11:14 +1100

    #256 remove fakes related test from test assembly as it is not supported on build server (community edition)

--- main/OpenCover.Test/Samples/SimpleMsTest.cs
+++ main/OpenCover.Test/Samples/SimpleMsTest.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.QualityTools.Testing.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenCover.Test.Framework.Persistance;
 
@@ -23,33 +22,6 @@ namespace OpenCover.Test.Samples
                 fixture.SetUp();
                 mi.Invoke(fixture, null);
                 fixture.TearDown();
-            }
-        }
-    }
-
-    public class MyTime
-    {
-        public int GetTheCurrentYear()
-        {
-            return DateTime.Now.Year;
-        }
-    }
-
-    [TestClass]
-    public class FakesMsTest
-    {
-        [TestMethod]
-        public void GetTheCurrentYear_When_2016()
-        {
-            // Shims can be used only in a ShimsContext:
-            using (ShimsContext.Create())
-            {
-                // Arrange:
-                // Shim DateTime.Now to return a fixed date:
-                System.Fakes.ShimDateTime.NowGet =
-                  () => new DateTime(2016, 1, 1);
-
-                Assert.AreEqual(2016, (new MyTime()).GetTheCurrentYear());
             }
         }
     }
