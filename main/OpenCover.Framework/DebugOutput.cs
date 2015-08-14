280ab36484d8e092e051678cd457f379fba506e8
Sam <merge13@gmx.net>
2015-08-14 13:29:36 +0200

    Directly use Windows API to write to debug output from managed code.

--- main/OpenCover.Framework/DebugOutput.cs
+++ main/OpenCover.Framework/DebugOutput.cs
@@ -1,12 +1,15 @@
-﻿using System.Diagnostics;
+﻿using System.Runtime.InteropServices;
 
 namespace OpenCover.Framework
 {
 	internal static class DebugOutput
 	{
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+		private static extern void OutputDebugString(string message);
+
 		public static void Print(string message)
 		{
-			Debug.WriteLine(string.Format("OpenCover: {0}", message));
+			OutputDebugString(string.Format("OpenCover: {0}", message));
 		}
 	}
 }
