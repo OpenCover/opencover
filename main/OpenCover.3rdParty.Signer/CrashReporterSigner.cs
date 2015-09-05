2eb03df5dc4ba6b39861e0dfe90e7f636f96dada
sawilde <shaun_wilde@hotmail.com>
2015-09-05 12:55:29 +1000

    move signer project sign crash reporter

---
+++ main/OpenCover.3rdParty.Signer/CrashReporterSigner.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using Mono.Cecil;
+
+namespace OpenCover.ThirdParty.Signer
+{
+    internal static class CrashReporterSigner
+    {
+
+        private const string Version = "1.5";
+
+        private static readonly string GendarmeAssemblyName = string.Format("CrashReporterdotNet.{0}", Version);
+
+        public static readonly string TargetFolder = Path.Combine("..", "tools", "CrashReporterSigned");
+        private static readonly string SourceFolder = Path.Combine("packages", GendarmeAssemblyName, "lib", "net20");
+        private static readonly string StrongNameKey = Path.Combine("..", "build", "Version", "opencover.3rdparty.snk");
+
+
+        public static bool AlreadySigned(string baseFolder)
+        {
+            var frameworkAssembly = Path.Combine(baseFolder, TargetFolder, "CrashReporter.NET.dll");
+            if (File.Exists(frameworkAssembly))
+            {
+                try
+                {
+                    var frameworkDefinition = AssemblyDefinition.ReadAssembly(frameworkAssembly);
+                    return frameworkDefinition.Name.HasPublicKey;
+                }
+                catch
+                {
+                }
+            }
+            return false;
+        }
+
+        public static void SignAssembly(string baseFolder)
+        {
+            var key = Path.Combine(baseFolder, StrongNameKey);
+            var assembly = Path.Combine(baseFolder, SourceFolder, "CrashReporter.NET.dll");
+            var newAssembly = Path.Combine(baseFolder, TargetFolder, "CrashReporter.NET.dll");
+
+            assembly = Path.GetFullPath(assembly);
+            newAssembly = Path.GetFullPath(newAssembly);
+
+            File.Copy(assembly, newAssembly, true);
+            var definition = AssemblyDefinition.ReadAssembly(newAssembly);
+            var keyPair = new StrongNameKeyPair(new FileStream(key, FileMode.Open, FileAccess.Read));
+            definition.Write(newAssembly, new WriterParameters() { StrongNameKeyPair = keyPair });
+        }
+    }
+}
---
+++ main/OpenCover.3rdParty.Signer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace OpenCover.ThirdParty.Signer
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+			var assemblyLocation = Assembly.GetAssembly (typeof(Program)).Location;
+			var assemblyFolder = Path.GetDirectoryName(assemblyLocation) ?? string.Empty;
+            var baseFolder = Path.Combine(assemblyFolder, "..", "..", "..");
+
+            SignGendarme(baseFolder);
+            SignCrashReporter(baseFolder);
+        }
+
+        private static void SignGendarme(string baseFolder)
+        {
+            var targetDirectory = Path.Combine(baseFolder, GendarmeSigner.TargetFolder);
+            if (!Directory.Exists(targetDirectory))
+                Directory.CreateDirectory(targetDirectory);
+
+            if (GendarmeSigner.AlreadySigned(baseFolder))
+            {
+                Console.WriteLine("Gendarme Framework is already Signed");
+                return;
+            }
+
+            Console.WriteLine("Signing Gendarme Framework");
+            GendarmeSigner.SignGendarmeFramework(baseFolder);
+
+            Console.WriteLine("Signing Gendarme Rules Maintainability");
+            GendarmeSigner.SignGendarmeRulesMaintainability(baseFolder);
+        }
+
+        private static void SignCrashReporter(string baseFolder)
+        {
+            var targetDirectory = Path.Combine(baseFolder, CrashReporterSigner.TargetFolder);
+            if (!Directory.Exists(targetDirectory))
+                Directory.CreateDirectory(targetDirectory);
+
+            if (CrashReporterSigner.AlreadySigned(baseFolder))
+            {
+                Console.WriteLine("CrashReporter is already Signed");
+                return;
+            }
+
+            Console.WriteLine("Signing CrashReporter");
+            CrashReporterSigner.SignAssembly(baseFolder);
+
+        }
+    }
+}
