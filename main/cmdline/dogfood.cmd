ac25cabb2ff3550ef1f32e1ca9aeeb386d63499a
ddur <github@registratura.org>
2016-01-12 07:48:39 +0100

    Tested without appveyor

--- main/cmdline/dogfood.cmd
+++ main/cmdline/dogfood.cmd
@@ -1,7 +1,9 @@
 @echo off
-@set ExcludeAdminOnly=/exclude:AdminOnly
-@if "%appveyor%" == "True" set ExcludeAdminOnly=""
 pushd %cd%
+setlocal
+@set ExcludeAdminOnly=/exclude:AdminOnly
+@if "%appveyor%" == "True" set ExcludeAdminOnly=
 cd %~dp0
 OpenCover.Console.exe -register:user -target:..\..\..\main\packages\NUnit.Runners.2.6.4\tools\nunit-console-x86.exe -targetargs:"OpenCover.Test.dll /noshadow %ExcludeAdminOnly%" -filter:"+[Open*]* -[OpenCover.T*]* -<nunit-console*>[*]* -<pdb*>[*]*" -output:opencovertests.xml -communicationtimeout:9999
+endlocal
 popd
