c821cdb54b4d27a3ede2104ec8fada5210b1ad9a
ddur <github@registratura.org>
2016-01-12 06:36:52 +0100

    cmd used by dogfood-release

--- main/cmdline/dogfood_exattr.cmd
+++ main/cmdline/dogfood_exattr.cmd
@@ -1,4 +1,6 @@
 @echo off
+@set ExcludeAdminOnly=/exclude:AdminOnly
+@if "%appveyor%" == "True" set ExcludeAdminOnly=""
 pushd %cd%
 cd %~dp0
 OpenCover.Console.exe -register:user -target:..\..\..\main\packages\NUnit.Runners.2.6.4\tools\nunit-console-x86.exe -targetargs:"OpenCover.Test.dll /noshadow %ExcludeAdminOnly%" -excludebyattribute:*.ExcludeFromCoverageAttribute -filter:"+[Open*]* -[OpenCover.T*]* -{nunit-console*}[*]* -{pdb*}[*]*" -output:opencovertests.xml -mergebyhash
