813876440e5ed4882492fd59bbe36b37e0dd3d92
ddur <github@registratura.org>
2016-01-12 06:42:52 +0100

    Restore by dogfood-release unused files

--- main/cmdline/dogfood_filter.cmd
+++ main/cmdline/dogfood_filter.cmd
@@ -2,5 +2,5 @@
 pushd %cd%
 cd %~dp0
 REM cover framework only
-OpenCover.Console.exe -register:user -target:..\..\..\main\packages\NUnit.Runners.2.6.4\tools\nunit-console-x86.exe -targetargs:"OpenCover.Test.dll /noshadow %ExcludeAdminOnly%" -filter:"+[Open*]OpenCover.Framework* -[OpenCover.T*]* -{nunit-console*}[*]* -{pdb*}[*]*" -output:opencovertests-framework-filter.xml -communicationtimeout:9999
+OpenCover.Console.exe -register:user -target:..\..\..\main\packages\NUnit.Runners.2.6.4\tools\nunit-console-x86.exe -targetargs:"OpenCover.Test.dll /noshadow /exclude:AdminOnly" -filter:"+[Open*]OpenCover.Framework* -[OpenCover.T*]* -{nunit-console*}[*]* -{pdb*}[*]*" -output:opencovertests-framework-filter.xml -communicationtimeout:9999
 popd
