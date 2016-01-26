0a4e2a0e46c5210c8bf9887b08ba8694527f73a3
ddur <github@registratura.org>
2016-01-26 23:27:52 +0100

    SonarQube: prevent reporting false issue
    Line 730 is not commented code.

--- main/OpenCover.Framework/Persistance/BasePersistance.cs
+++ main/OpenCover.Framework/Persistance/BasePersistance.cs
@@ -727,8 +727,8 @@ namespace OpenCover.Framework.Persistance
                             // Never found 1 character sequencePoint except "{" and "}"
                             // Never found 2 character sequencePoint except "in" keyword
                             // Afaik, c# cannot express branch condition in one or two characters of source code
-                            // "x|y" "if(x)" "while(x)" "switch(x){...}" "case:" "x?." "x??" "x==y" "x?y:z;" "for(...)" "foreach(...)" "x is y"
-                            // "do" keyword does not generate SequencePoint 
+                            // Examples x|y if(x) while(x) switch(x){...} case: x?. x?? x==y x?y:z; for(...) foreach(...) x is y
+                            // Keyword "do" does not generate SequencePoint 
                             sp.BranchPoints = new List<BranchPoint>();
                         }
                     }
