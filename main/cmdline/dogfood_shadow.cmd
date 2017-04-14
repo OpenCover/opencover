4a2c067067ce62318db15521543a871854dafb81
Shaun Wilde <shaun_wilde@hotmail.com>
2017-04-14 17:15:32 +1000

    fix tooling changes

--- main/cmdline/dogfood_shadow.cmd
+++ main/cmdline/dogfood_shadow.cmd
@@ -1,2 +1,2 @@
 rem run dogfood tests with shadow
-OpenCover.Console.exe -register:user -targetdir:"%cd%" -target:..\..\..\main\packages\NUnit.ConsoleRunner.3.2.1\tools\nunit3-console.exe -targetargs:"OpenCover.Test.dll --where:cat!=AdminOnly --x86" -filter:"+[Open*]* -[OpenCover.T*]*" -output:opencovertests.xml
+OpenCover.Console.exe -register:user -targetdir:"%cd%" -target:..\..\..\main\packages\NUnit.ConsoleRunner.3.5.0\tools\nunit3-console.exe -targetargs:"OpenCover.Test.dll --where:cat!=AdminOnly --x86" -filter:"+[Open*]* -[OpenCover.T*]*" -output:opencovertests.xml
