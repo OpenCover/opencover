d945f4af19d36ce15291e5dd94048d9a2e0942ca
davids_sackstein <davids@codeprecise.com>
2017-05-08 07:50:27 +0300

    Added logger for C++

---
+++ main/Logger/ILogger.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class ILogger
+{
+public:
+	virtual ~ILogger() {};
+	virtual void Log(const char*, ...) = 0;
+};
+
+__declspec (dllexport) ILogger& GetFileLogger(const char* fileName);
+__declspec (dllexport) ILogger& GetTraceLogger();
