3587d46a3b4fa08ba29937ffdda4465da4d9fa7a
davids_sackstein <davids@codeprecise.com>
2017-05-08 09:07:37 +0300

    Simpler target application

---
+++ main/OpenCover.Simple.Target/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Target
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public int Height { get; set; }
+
+        public override string ToString()
+        {
+            return $"Age={Age} Height={Height}";
+        }
+    }
+}
