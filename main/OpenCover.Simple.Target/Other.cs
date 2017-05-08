dbda9c67148fc3027dbb1a7c6897cc8c1d8dd0cb
davids_sackstein <davids@codeprecise.com>
2017-05-08 23:35:05 +0300

    reverted OpenCover.Simple.Target

---
+++ main/OpenCover.Simple.Target/Other.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Namespace
+{
+    public class Other
+    {
+        public Other()
+        {
+        }
+
+        public int Data { get; set; }
+
+        private int _x;
+
+        public int X { get { return _x; } }
+    }
+}
