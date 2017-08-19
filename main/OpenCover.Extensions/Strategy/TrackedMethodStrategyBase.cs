bbe2d4cac3fd275c3048f572295ec9134b359a4d
Shaun Wilde <shaun_wilde@hotmail.com>
2017-08-19 16:45:35 +1000

    #742 support nested test classes and methods

--- main/OpenCover.Extensions/Strategy/TrackedMethodStrategyBase.cs
+++ main/OpenCover.Extensions/Strategy/TrackedMethodStrategyBase.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Mono.Cecil;
 using OpenCover.Framework.Model;
@@ -8,39 +9,55 @@ namespace OpenCover.Extensions.Strategy
 {
     public abstract class TrackedMethodStrategyBase : ITrackedMethodStrategy
     {
-        private readonly ISet<string> acceptedAttributes;
+        private readonly ISet<string> _acceptedAttributes;
 
-        public string StrategyName { get; private set; }
+        public string StrategyName { get; }
 
         protected TrackedMethodStrategyBase(string strategyName, IEnumerable<string> attributeNames)
         {
             StrategyName = strategyName;
-            acceptedAttributes = new HashSet<string>(attributeNames);
+            _acceptedAttributes = new HashSet<string>(attributeNames);
         }
 
         protected TrackedMethodStrategyBase(string strategyName, string attribute)
         {
             StrategyName = strategyName;
-            acceptedAttributes = new HashSet<string> { attribute };
+            _acceptedAttributes = new HashSet<string> { attribute };
         }
 
         protected IEnumerable<TrackedMethod> GetTrackedMethodsByAttribute(IEnumerable<TypeDefinition> typeDefinitions)
         {
             return (from typeDefinition in typeDefinitions
-                    from methodDefinition in typeDefinition.Methods
-                    from customAttribute in methodDefinition.CustomAttributes
-                    where acceptedAttributes.Contains(customAttribute.AttributeType.FullName)
-                    select new TrackedMethod
-                    {
-                        MetadataToken = methodDefinition.MetadataToken.ToInt32(),
-                        FullName = methodDefinition.FullName,
-                        Strategy = StrategyName
-                    });
+                from methodDefinition in typeDefinition.Methods
+                from customAttribute in methodDefinition.CustomAttributes
+                where _acceptedAttributes.Contains(customAttribute.AttributeType.FullName)
+                select new TrackedMethod
+                {
+                    MetadataToken = methodDefinition.MetadataToken.ToInt32(),
+                    FullName = methodDefinition.FullName,
+                    Strategy = StrategyName
+                });
         }
 
         public IEnumerable<TrackedMethod> GetTrackedMethods(IEnumerable<TypeDefinition> typeDefinitions)
         {
-            return GetTrackedMethodsByAttribute(typeDefinitions);
+            var allTypes = GetAllTypes(typeDefinitions);
+
+            return GetTrackedMethodsByAttribute(allTypes);
+        }
+
+        private static IEnumerable<TypeDefinition> GetAllTypes(IEnumerable<TypeDefinition> typeDefinitions)
+        {
+            var types = new List<TypeDefinition>();
+            foreach (var typeDefinition in typeDefinitions)
+            {
+                types.Add(typeDefinition);
+                if (typeDefinition.HasNestedTypes)
+                {
+                    types.AddRange(GetAllTypes(typeDefinition.NestedTypes));
+                }
+            }
+            return types;
         }
     }
 }
