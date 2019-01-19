2399786d7b3e69ccf56f806b8836774ea993f42a
Shaun Wilde <shaun_wilde@hotmail.com>
2019-01-19 11:21:54 +1100

    Update tools via nuget packages
    These packages are used by the build process

--- main/cmdline/opencover_merge.cmd
+++ main/cmdline/opencover_merge.cmd
@@ -1,3 +1,3 @@
-OpenCover.Console.exe -register:user -target:"c:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" -targetargs:"/testcontainer:OpenCover.Test.dll /noisolation" -filter:"+[Open*]* -[OpenCover.T*]*" -mergebyhash -output:opencovertests.xml 
-OpenCover.Console.exe -register:user -target:"..\..\..\main\packages\xunit.runner.console.2.1.0\tools\xunit.console.x86.exe" -targetargs:"OpenCover.Test.dll -noshadow" -filter:"+[Open*]* -[OpenCover.T*]*" -mergebyhash -output:opencovertests.xml -mergeoutput
+OpenCover.Console.exe -register:user -target:"%programfiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" -targetargs:"/testcontainer:OpenCover.Test.dll /noisolation" -filter:"+[Open*]* -[OpenCover.T*]*" -mergebyhash -output:opencovertests.xml 
+OpenCover.Console.exe -register:user -target:"..\..\..\main\packages\xunit.runner.console.2.4.1\tools\xunit.console.x86.exe" -targetargs:"OpenCover.Test.dll -noshadow" -filter:"+[Open*]* -[OpenCover.T*]*" -mergebyhash -output:opencovertests.xml -mergeoutput
 
