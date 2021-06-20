fb5e25971a439f03a750dcc98d4b2cc8108b9fb7
Shaun Wilde <shaun_wilde@hotmail.com>
2021-06-20 11:44:24 +1000

    update nuget packages

--- main/cmdline/dogfood_exattr.cmd
+++ main/cmdline/dogfood_exattr.cmd
@@ -4,6 +4,6 @@ setlocal
 @set ExcludeAdminOnly=--where:cat!=AdminOnly
 @if "%appveyor%" == "True" set ExcludeAdminOnly=
 cd %~dp0
-OpenCover.Console.exe -register:user -target:..\..\..\main\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe -targetargs:"OpenCover.Test.dll --x86 %ExcludeAdminOnly%" -excludebyattribute:*.ExcludeFromCoverageAttribute -filter:"+[Open*]* -[OpenCover.T*]* -{nunit-console*}[*]* -{pdb*}[*]*" -output:opencovertests.xml -mergebyhash
+OpenCover.Console.exe -register:user -target:..\..\..\main\packages\NUnit.ConsoleRunner.3.12.0\tools\nunit3-console.exe -targetargs:"OpenCover.Test.dll --x86 %ExcludeAdminOnly%" -excludebyattribute:*.ExcludeFromCoverageAttribute -filter:"+[Open*]* -[OpenCover.T*]* -{nunit-console*}[*]* -{pdb*}[*]*" -output:opencovertests.xml -mergebyhash
 endlocal
 popd
--- main/cmdline/dogfood_threshold.cmd
+++ main/cmdline/dogfood_threshold.cmd
@@ -1,1 +1,1 @@
-OpenCover.Console.exe -register:user -threshold:1 -target:..\..\..\main\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe -targetargs:"OpenCover.Test.dll --where:cat!=AdminOnly --x86" -filter:"+[Open*]* -[OpenCover.T*]*" -output:opencovertests.xml
+OpenCover.Console.exe -register:user -threshold:1 -target:..\..\..\main\packages\NUnit.ConsoleRunner.3.12.0\tools\nunit3-console.exe -targetargs:"OpenCover.Test.dll --where:cat!=AdminOnly --x86" -filter:"+[Open*]* -[OpenCover.T*]*" -output:opencovertests.xml
